34e23d719424424c1a4ab4c5ecc4f26b41da5fe6
Judi <127020548+judi1968@users.noreply.github.com>
2024-02-11 15:37:04 +0300

    finish home

---
+++ vue-project/src/views/Musician-net/Ajout.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>ajout</p>
+    </div>
+</template>
--- vue-project/src/views/Musician-net/Home.vue
+++ vue-project/src/views/Musician-net/Home.vue
@@ -1,5 +1,22 @@
+<script>
+   import './../../assets/home.css' 
+   import './../../assets/product.css'
+</script>
 <template>
-    <div>
-        <h1 class="btn">Bonjour</h1>
+    <div class="home">
+        <div class="img position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5 texto">
+      <h1 class="display-10 fw-normal">.</h1>
+
+      <p class="lead fw-normal">.</p>
+      <p class="lead fw-normal">.</p>
+
+      <p class="lead fw-normal">.</p>
+
+      <p class="lead fw-normal">.</p>
+
+
+    </div>
+  </div>
     </div>
 </template>
